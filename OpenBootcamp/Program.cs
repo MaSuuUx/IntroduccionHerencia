eaa85216ddd503bb196f0a81b4a5b66bc8bfd395
Marcelo Monge <111376835+MaSuuUx@users.noreply.github.com>
2023-07-06 20:41:31 -0600

    Add files via upload

---
+++ OpenBootcamp/Program.cs
@@ -0,0 +1,35 @@
+﻿//Herencia
+
+Vehiculo vehiculo = new Vehiculo();
+vehiculo.Arrancar();
+
+
+Coche coche = new Coche();
+coche.Arrancar();
+coche.DetenerCoche();
+
+
+//Clase Parent
+class Vehiculo 
+{
+    public double gasolina { get; set; }
+    public int aforo { get; set; }
+
+
+    public void Arrancar()
+    {
+        Console.WriteLine("Este vehiculo ha arrancado");
+    }
+}
+
+//Clase Child
+class Coche : Vehiculo
+{
+    public int ruedas { get; set; }
+
+    public void DetenerCoche()
+    {
+        Console.WriteLine("El coche se ha detenido");
+
+    }
+}
